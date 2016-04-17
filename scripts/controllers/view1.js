4bca51a881d905c403e2974e62bbbb2c31262c38
obama <baral.oh16@gmail.com>
2016-04-18 02:13:17 +0300

    e

--- scripts/controllers/view1.js
+++ scripts/controllers/view1.js
@@ -10,7 +10,17 @@ angular.module('myApp', [])
   $scope.info = {};
   $scope.defences = {};
   $scope.auto = {};
-  $scope.teleop = {};
+  $scope.teleop = {
+    crosses_low_bar: 0,
+    crosses_A: 0,
+    crosses_B: 0,
+    crosses_C: 0,
+    crosses_D: 0,
+    scores_high: 0,
+    scores_low: 0,
+    tries_high:0,
+    tries_low:0
+  };
   $scope.end_game = {};
   $scope.general = {};
 
@@ -18,43 +28,39 @@ angular.module('myApp', [])
     var blob = new Blob(["Hello, world!", '53252'], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "hello world.txt");
     $scope.a = "end!";
-  }
+  };
 
   $scope.setDefence = function(category, value){
-      console.log(category + ': ' + value)
     $scope.defences[category] = value;
-  }
+  };
 
   $scope.getDefence = function(category){
     return $scope.defences[category];
-  }
+  };
 
   $scope.setMissionAuto = function(mission, value){
-    console.log(mission + ': ' + value)
     $scope.auto[mission] = value;
-  }
+  };
 
   $scope.getMissionAuto = function(mission){
     return $scope.auto[mission];
-  }
+  };
 
   $scope.setMissionEndGame = function(mission, value){
-    console.log(mission + ': ' + value)
     $scope.end_game[mission] = value;
-  }
+  };
 
   $scope.getMissionEndGame = function(mission){
     return $scope.end_game[mission];
-  }
+  };
 
   $scope.setGeneral = function(key, value){
-    console.log(key + ': ' + value)
     $scope.general[key] = value;
-  }
+  };
 
   $scope.getGeneral = function(key){
     return $scope.general[key];
-  }
+  };
 
   $scope.print = function(){
 
@@ -68,7 +74,12 @@ angular.module('myApp', [])
     to_print = "\"" + to_print.join("\",\"") + "\"";
     var blob = new Blob([to_print], {type: "text/csv;charset=utf-8"});
     saveAs(blob, "hello world.csv");
-  }
+  };
+
+
+  $scope.$watchGroup(['info', 'defences', 'auto', 'teleop','end_game', 'general' ], function(){
+    console.log(345);
+  });
 
 
 
