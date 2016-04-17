b8ba245ffb4691d188bbc536eea6d825039f348c
obama <baral.oh16@gmail.com>
2016-04-18 01:07:35 +0300

    e

--- scripts/controllers/view1.js
+++ scripts/controllers/view1.js
@@ -15,7 +15,7 @@ angular.module('myApp', [])
   $scope.general = {};
 
   $scope.add = function(){
-    var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+    var blob = new Blob(["Hello, world!", '53252'], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "hello world.txt");
     $scope.a = "end!";
   }
@@ -57,8 +57,17 @@ angular.module('myApp', [])
   }
 
   $scope.print = function(){
-    var to_print = [info.name, info.team_number];
-    debugger;
+
+    var to_print = [$scope.info.scouter_name, $scope.info.team_number, $scope.info.scouter_name, $scope.defences.A, $scope.defences.B,
+        $scope.defences.C, $scope.defences.D, $scope.auto.reached, $scope.auto.crroses, $scope.auto.score,
+        $scope.teleop.crosses_low_bar, $scope.teleop.crosses_A, $scope.teleop.crosses_B, $scope.teleop.crosses_C,
+        $scope.teleop.crosses_D, $scope.teleop.scores_low, $scope.teleop.tries_low, $scope.teleop.scores_high,
+        $scope.teleop.tries_high, $scope.end_game.scale_try, $scope.end_game.scale, $scope.general.defenc_robot,
+        $scope.general.comment];
+
+    to_print = "\"" + to_print.join("\",\"") + "\"";
+    var blob = new Blob([to_print], {type: "text/csv;charset=utf-8"});
+    saveAs(blob, "hello world.csv");
   }
 
 
