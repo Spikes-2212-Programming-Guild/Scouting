c46db947478796312ae4c02d5e64ba81b9bb2870
obama <baral.oh16@gmail.com>
2016-04-18 00:41:38 +0300

    e

--- scripts/controllers/view1.js
+++ scripts/controllers/view1.js
@@ -5,9 +5,14 @@ angular.module('myApp', [])
 
 
 .controller('View1Ctrl',[ '$scope', function($scope) {
-  $scope.line = {};
-  $scope.a = 4;
+  $scope.lines = [];
+
+  $scope.info = {};
   $scope.defences = {};
+  $scope.auto = {};
+  $scope.teleop = {};
+  $scope.end_game = {};
+  $scope.general = {};
 
   $scope.add = function(){
     var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
@@ -15,13 +20,48 @@ angular.module('myApp', [])
     $scope.a = "end!";
   }
 
-  $scope.setDefence = function(defence, value){
-    $scope.defences[defence] = value;
+  $scope.setDefence = function(category, value){
+      console.log(category + ': ' + value)
+    $scope.defences[category] = value;
   }
 
-  $scope.getDefence = function(defence){
-    return $scope.defences[defence];
+  $scope.getDefence = function(category){
+    return $scope.defences[category];
   }
+
+  $scope.setMissionAuto = function(mission, value){
+    console.log(mission + ': ' + value)
+    $scope.auto[mission] = value;
+  }
+
+  $scope.getMissionAuto = function(mission){
+    return $scope.auto[mission];
+  }
+
+  $scope.setMissionEndGame = function(mission, value){
+    console.log(mission + ': ' + value)
+    $scope.end_game[mission] = value;
+  }
+
+  $scope.getMissionEndGame = function(mission){
+    return $scope.end_game[mission];
+  }
+
+  $scope.setGeneral = function(key, value){
+    console.log(key + ': ' + value)
+    $scope.general[key] = value;
+  }
+
+  $scope.getGeneral = function(key){
+    return $scope.general[key];
+  }
+
+  $scope.print = function(){
+    var to_print = [info.name, info.team_number];
+    debugger;
+  }
+
+
 
 
 
